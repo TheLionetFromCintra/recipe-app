dabad9efcea606a1a017059d19b095cc38aee268
theLionetFromCintra <vinipyx34@bk.ru>
2022-06-23 21:51:29 +0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -105,6 +105,11 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const welcomeMess = function () {
+  console.log("Hi");
+};
+welcomeMess();
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
